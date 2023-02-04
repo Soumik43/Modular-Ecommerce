f15281d607203eaa5cc9963ff1c9b0ef05d6d87b
Soumik Paul <soumik.paul@contentstack.com>
2023-02-04 11:51:55 +0530

    Hosted the static data JSON and utilized it

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ root.appendChild(new Cart(totalCartCount).render());
 const cardContainer = document.createElement("div");
 cardContainer.id = "rootCardContainer";
 
-fetch("https://dummyjson.com/products")
+fetch("https://raw.githubusercontent.com/Soumik43/Modular-Ecommerce/main/data/products.json")
     .then((res) => res.json())
     .then((data) => {
         data.products.forEach((cardData) => {
